1e8fc1438b0aafa9c77ec5fbdb2f0153628132cf
rezkirahman <rezkirahman1996@gmail.com>
2022-12-05 02:02:35 +0700

    railway

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -15,18 +15,17 @@ var DB_USER = os.Getenv("DB_USER")
 var DB_PASSWORD = os.Getenv("DB_PASSWORD")
 var DB_NAME = os.Getenv("DB_NAME")
 var DB_PORT = os.Getenv("DB_PORT")
+
 // Connection Database
 func DatabaseInit() {
-  var err error
-  //dsn := "root:@tcp(127.0.0.1:3306)/waysbucks?charset=utf8mb4&parseTime=True&loc=Local"
-  dsn:= fmt.Sprintf("%s:%s@tcp(%s:%s)%s?charset=utf8mb4&parseTime=True&loc=Local",DB_USER,DB_PASSWORD,DB_HOST,DB_PORT,DB_NAME)
-  DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	var err error
+	//dsn := "root:@tcp(127.0.0.1:3306)/waysbucks?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)%s?charset=utf8mb4&parseTime=True&loc=Local", DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 
-  fmt.Println("Connected to Database")
+	fmt.Println("Connected to Database")
 }
-
-
