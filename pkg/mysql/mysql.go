9bc314986874e7154f5f031c171ea645f0af1d13
rezkirahman <rezkirahman1996@gmail.com>
2022-12-05 03:46:07 +0700

    deploy

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -10,17 +10,17 @@ import (
 
 var DB *gorm.DB
 
-var DB_HOST = os.Getenv("DB_HOST")
-var DB_USER = os.Getenv("DB_USER")
-var DB_PASSWORD = os.Getenv("DB_PASSWORD")
-var DB_NAME = os.Getenv("DB_NAME")
-var DB_PORT = os.Getenv("DB_PORT")
-
 // Connection Database
 func DatabaseInit() {
 	var err error
 	//dsn := "root:@tcp(127.0.0.1:3306)/waysbucks?charset=utf8mb4&parseTime=True&loc=Local"
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
+	var DB_HOST = os.Getenv("DB_HOST")
+	var DB_USER = os.Getenv("DB_USER")
+	var DB_PASSWORD = os.Getenv("DB_PASSWORD")
+	var DB_NAME = os.Getenv("DB_NAME")
+	var DB_PORT = os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
   //dsn := "root:eIoIW3ErHby3rxGFR9vK@tcp(containers-us-west-146.railway.app:6648)/railway?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	
