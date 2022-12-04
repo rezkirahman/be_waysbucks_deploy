422c5d9a298eee81d43ffe37b076b1e19bed7ac2
rezkirahman <rezkirahman1996@gmail.com>
2022-12-05 02:14:12 +0700

    railway

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -20,12 +20,17 @@ var DB_PORT = os.Getenv("DB_PORT")
 func DatabaseInit() {
 	var err error
 	//dsn := "root:@tcp(127.0.0.1:3306)/waysbucks?charset=utf8mb4&parseTime=True&loc=Local"
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)%s?charset=utf8mb4&parseTime=True&loc=Local", DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
+	//dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
+	dsn := "root:eIoIW3ErHby3rxGFR9vK@tcp(containers-us-west-146.railway.app:6648)/railway?charset=utf8mb4&parseTime=True&loc=Local"
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println("Connected to Database")
 }
+// DB_HOST=containers-us-west-146.railway.app
+// DB_USER=root
+// DB_PASSWORD=eIoIW3ErHby3rxGFR9vK
+// DB_NAME=railway
+// DB_PORT=6648
